5fc756c17f5d19f84be46a6225b42b51cebf9e05
Shashank <34152717+iThink32@users.noreply.github.com>
2019-02-05 18:36:16 +0530

    Add files via upload

---
+++ GenericOperation.swift
@@ -0,0 +1,71 @@
+//
+//  GenericOperation.swift
+//  TestOperationQueue
+//
+//  Created by N.A Shashank on 31/01/19.
+//  Copyright © 2019 Razorpay. All rights reserved.
+//
+
+import Foundation
+
+class GenericOperation:Operation {
+    
+    typealias CompletionCallBack = () -> Void
+    typealias TaskType = (@escaping CompletionCallBack) -> Void
+//    let dataModel:T
+    let closureTask:TaskType
+
+    init(closureTask:@escaping TaskType) {
+        self.closureTask = closureTask
+//        self.dataModel = dataModel
+        // a little concept here , here Operation does not override init() of NSObject neither does Generic Operation so first init(closure.... initializes the subclass and then calls its superclass init() which is ultimately NSOBject's init() ,that is why we first have to call super.init() before self.completionBlock=...
+        //super.init()
+        //self.completionBlock = completionBlock
+    }
+    
+    var isCurrentlyExecuting = false
+    
+    override var isExecuting: Bool {
+        set{
+            willChangeValue(for: \GenericOperation.isExecuting)
+            self.isCurrentlyExecuting = newValue
+            didChangeValue(for: \GenericOperation.isExecuting)
+        } get{
+            return isCurrentlyExecuting
+        }
+    }
+    
+    var isCurrentlyFinished = false
+    
+    override var isFinished: Bool {
+        set{
+            willChangeValue(for: \GenericOperation.isFinished)
+            self.isCurrentlyFinished = newValue
+            didChangeValue(for: \GenericOperation.isFinished)
+        } get{
+            return self.isCurrentlyFinished
+        }
+    }
+    
+    
+    override func main() {
+        
+        //check if cancelled
+        guard isCancelled == false else{
+            return
+        }
+        
+        self.isExecuting = true
+        //perform task
+        self.closureTask(){[weak self] in
+            self?.isExecuting = false
+            self?.isFinished = true
+        }
+        
+    }
+    deinit {
+        print("\(self) deallocated")
+    }
+    
+    
+}
---
+++ OperationQueueManager.swift
@@ -0,0 +1,65 @@
+//
+//  OperationQueueManager.swift
+//  TestOperationQueue
+//
+//  Created by N.A Shashank on 31/01/19.
+//  Copyright © 2019 Razorpay. All rights reserved.
+//
+
+import Foundation
+
+class OperationQueueManager {
+    
+    private lazy var dictOperations:[AnyHashable:[Operation]] = [AnyHashable:[Operation]]()
+    private lazy var operationQueue:OperationQueue = {
+        var queue = OperationQueue()
+        queue.name = "Generic Operation Queue"
+        return queue
+    }()
+    
+    @discardableResult func cancelOperations(index:AnyHashable) -> Bool {
+        guard let arrOperations = self.dictOperations[index] else{
+            return false
+        }
+        arrOperations.forEach { (operation) in
+            operation.cancel()
+        }
+        self.dictOperations[index] = nil
+        return true
+    }
+    
+    @discardableResult func addOperationToQueue(op:[Operation],index:AnyHashable) -> Bool {
+        guard dictOperations[index] == nil else{
+            return false
+        }
+        op.forEach {[weak self] (operation) in
+            self?.operationQueue.addOperation(operation)
+        }
+        self.dictOperations[index] = op
+        return true
+    }
+
+    /// This func is used to generate dependencies between operation based on a string.
+    ///
+    /// - Parameters:
+    ///   - dependencyString:It has the format op1->op2->op3 where op1,op2 and op3 are the operations
+    ///   and the arrows represent its dependency.Do not add spaces anywhere in the string
+    ///   - dictValues: a dictionary specifying the operations corresponding to its keys for
+    ///   eg: "op1":OperationInstance
+    /// - Returns:True if it is successfully able to add dependencies false if not
+    func addDependenenciesBasedOn(dependencyString:String,dictValues:[String:Operation]) -> Bool {
+        // parse components
+        let arrComponents = dependencyString.components(separatedBy: ",")
+        for strComponent in arrComponents {
+            let operationComponents = strComponent.components(separatedBy: "->")
+            guard operationComponents.count == 2,let operationOne = dictValues[operationComponents[0]],let operationTwo = dictValues[operationComponents[1]] else{
+                return false
+            }
+            operationOne.addDependency(operationTwo)
+            self.operationQueue.addOperations([operationTwo,operationOne], waitUntilFinished: false)
+        }
+        return true
+    }
+    
+    
+}
